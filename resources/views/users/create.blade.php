5c614ccec4a66169cf39e0790467068474a47550
Double <double6427@163.com>
2018-12-20 07:35:41 +0000

    增加 CSRF 验证

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -29,6 +29,10 @@
             <input type="password" name="password_confirmation" class="form-control" value="{{ old('password_confirmation') }}">
           </div>
 
+          <div class="form-group">
+            {{ csrf_field() }}
+          </div>
+          
           <button type="submit" class="btn btn-primary">注册</button>
       </form>
     </div>
